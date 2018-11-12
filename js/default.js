11779be105b0256c4c75197414ad8f05d174b13f
Yossoy <32935678+Yossoy079@users.noreply.github.com>
2018-11-12 17:58:02 +0900

    test

--- js/default.js
+++ js/default.js
@@ -16,19 +16,19 @@
 //}
 
 var number = Math.floor(Math.random() * 3);
-  var answer = parseInt(window.prompt('数あてゲーム。0~3の数字を入力してください！'));
-  var message;
-  if(answer === number){
+var answer = parseInt(window.prompt('数あてゲーム。0~3の数字を入力してください！'));
+var message;
+if(answer === number){
   message = '正解で～～す';
-  }
-  else if(answer < number){
- message = '答えはもっと大きい数字で～す'; 
-  }
-  else if(number < answer){
+}
+else if(answer < number){
+  message = '答えはもっと大きい数字で～す'; 
+}
+else if(number < answer){
   message = '答えはもっと小さい数字で～す'; 
-  }
-  else{
+}
+else{
   message = '0~3の数字を入力してください';
-  }
+}
 
-  document.getElementById('choice').textContent = message;
+document.getElementById('choice').textContent = message;

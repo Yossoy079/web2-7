d0902c4e14de32f671304aecaf2e106ecc2c7c13
Yossoy <32935678+Yossoy079@users.noreply.github.com>
2018-11-12 17:43:19 +0900

    test

--- js/default.js
+++ js/default.js
@@ -1,4 +1,38 @@
-window.alert('こんにちは世界！');
+//document.getElementById('choice').textContent = new Date();
 
-console.log('こんにちは世界！');
+//window.alert('こんにちは世界！');
 
+//console.log('こんにちは世界！');
+
+//var result = window.confirm('準備OK?');
+
+//document.getElementById('choice').textContent = result;
+
+//if(result){
+//  document.getElementById('choice').textContent = 'OKが押されました';
+//}
+//else{
+//  document.getElementById('choice').textContent = 'キャンセルが押されました';
+//}
+
+var number = Math.floor(Math.random() * 50);
+var answer = parseInt(window.prompt('数あてゲーム。0~50の数字を入力してください！'));
+
+var message;
+while(true){
+if(answer === number){
+  message = '正解で～～す';
+  break;
+}
+else if(answer < number){
+ message = '答えはもっと大きい数字で～す'; 
+}
+else if(number < answer){
+  message = '答えはもっと小さい数字で～す'; 
+}
+else{
+  message = '0~3の数字を入力してください';
+}
+
+document.getElementById('choice').textContent = message;
+}
